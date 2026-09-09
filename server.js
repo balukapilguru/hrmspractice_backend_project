68ad14689faf7f9aaca367eff00fa1db23086a23
balukapilguru <121420956+balukapilguru@users.noreply.github.com>
2026-09-09 10:22:17 +0530

    Add backend health endpoint

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("HRMS Backend Running 🚀");
+  res.send("HRMS Backend Running  ~@");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "HRMS Backend is healthy"
+  });
 });
 
 app.listen(5000, () => {
   console.log("Server started");
 });
+
+const db = require("./db");
